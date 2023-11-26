8134c8bbb49fcd12afb582c734373566945fd7a0
Raghav Dalmia <raghav912000@gmail.com>
2023-11-26 21:56:10 +0530

    init commit

---
+++ src/test/java/com/online/dukaan/onlinedukaan/OnlineDukaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.dukaan.onlinedukaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDukaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
